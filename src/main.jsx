462f6f1dffffee3c22ea244a3a1bc2d4e27c827b
NessieflakeS <nikandrof.danil@gmail.com>
2025-10-15 22:15:15 +0800

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-export default Main
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' 
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
